3b9d91d8a5d1089d446261e23979b148f0d301c2
Marek Vyleťal <marek.vyletal@student.sps-cl.cz>
2025-02-03 23:29:23 +0100

    Playr_Movement

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -5,10 +5,13 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     private string _horizontalAxis = "Horizontal", _verticalAxis = "Vertical";
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private Rigidbody2D _rb2D;
+
+    private Vector2 _input;
+    private void FixedUpdate()
     {
-        
+        _rb2D.linearVelocity = _input;
     }
 
     // Update is called once per frame
@@ -16,5 +19,6 @@ public class Player : MonoBehaviour
     {
         float HorizontalInput = Input.GetAxisRaw(_horizontalAxis);
         float VerticalInput = Input.GetAxisRaw(_verticalAxis);
+        _input = new Vector2(HorizontalInput, VerticalInput);
     }
 }
